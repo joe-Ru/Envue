d38b2b0398fed3354b080a2cf7d3c719e070be0d
joseph <ruizjoseph17@gmail.com>
2020-07-11 13:13:27 -0400

    Login component working

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+            return {
+                authenticated: false,
+                mockAccount: {
+                    username: "test",
+                    password: "test"
+                }
+            }
+        },
+        mounted() {
+            if(!this.authenticated) {
+                this.$router.replace({ name: "login" });
+            }
+        },
+        methods: {
+            setAuthenticated(status) {
+                this.authenticated = status;
+            },
+            logout() {
+                this.authenticated = false;
+            }
+        }
+};
+</script>
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Testing"/>
+  <HelloWorld msg="Testing" />
+    <div id="nav">
+      <router-link
+        v-if="authenticated"
+        to="/login"
+        v-on:click.native="logout()"
+        replace
+        >Logout</router-link
+      >
+    </div>
+    <router-view @authenticated="setAuthenticated" />
+    <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
